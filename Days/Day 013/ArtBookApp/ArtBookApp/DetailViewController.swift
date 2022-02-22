92ccd66f90af28b49d722122d8a754f4269b655d
Ali Kolukirik <aakolukirik@gmail.com>
2022-02-22 23:50:18 +0300

    Day 013

--- Days/Day 013/ArtBookApp/ArtBookApp/DetailViewController.swift
+++ Days/Day 013/ArtBookApp/ArtBookApp/DetailViewController.swift
@@ -6,16 +6,67 @@
 //
 
 import UIKit
+import CoreData
 
 class DetailViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-
+    
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var nameText: UITextField!
     @IBOutlet weak var artistText: UITextField!
     @IBOutlet weak var yearText: UITextField!
     
+    var chosenPainting = ""
+    var chosenPaintingId : UUID?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if chosenPainting != "" {
+            
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            let context = appDelegate.persistentContainer.viewContext
+            
+            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Paintings")
+            let idString = chosenPaintingId?.uuidString
+            fetchRequest.predicate = NSPredicate(format: "id = %@", idString!)
+            fetchRequest.returnsObjectsAsFaults = false
+            
+            do {
+                let results = try context.fetch(fetchRequest)
+                
+                if results.count > 0 {
+                    
+                    for result in results as! [NSManagedObject] {
+                        
+                        if let name = result.value(forKey: "name") as? String {
+                            nameText.text = name
+                        }
+                        
+                        if let artist = result.value(forKey: "artist") as? String {
+                            artistText.text = artist
+                        }
+                        
+                        if let year = result.value(forKey: "year") as? Int {
+                            yearText.text = String(year)
+                        }
+                        
+                        if let imageData = result.value(forKey: "image") as? Data {
+                            let image = UIImage(data: imageData)
+                            imageView.image = image
+                        }
+                    }
+                }
+                
+            } catch{
+                print("error :(((((((")
+            }
+            
+        } else {
+            nameText.text = ""
+            artistText.text = ""
+            yearText.text = ""
+        }
         
         let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
         view.addGestureRecognizer(gestureRecognizer)
@@ -23,12 +74,39 @@ class DetailViewController: UIViewController, UIImagePickerControllerDelegate, U
         imageView.isUserInteractionEnabled = true
         let tapGestureRegognizer = UITapGestureRecognizer(target: self, action: #selector(selectImage))
         imageView.addGestureRecognizer(tapGestureRegognizer)
-    
+        
     }
     
     @IBAction func saveButton(_ sender: Any) {
         
-        print("test")
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let newPainting = NSEntityDescription.insertNewObject(forEntityName: "Paintings", into: context)
+        
+        newPainting.setValue(nameText.text, forKey: "name")
+        newPainting.setValue(artistText.text, forKey: "artist")
+        newPainting.setValue(UUID(), forKey: "id")
+        
+        if let year = Int(yearText.text!){
+            newPainting.setValue(year, forKey: "year")
+        }
+        
+        let data = imageView.image?.jpegData(compressionQuality: 0.5)
+        
+        newPainting.setValue(data, forKey: "image")
+        
+        do {
+            try context.save()
+            print("uldi")
+        } catch  {
+            print("aglaaa")
+        }
+        
+        self.navigationController?.popViewController(animated: true)
+        
+        NotificationCenter.default.post(name: NSNotification.Name(rawValue: "newData"), object: nil)
+        
     }
     
     @objc func hideKeyboard() {
@@ -53,5 +131,5 @@ class DetailViewController: UIViewController, UIImagePickerControllerDelegate, U
         imageView.image = info[.originalImage]  as? UIImage        //info içerisine bir çok farklı fonskiyon çıkıyor bunları incele
         self.dismiss(animated: true, completion: nil)
     }
-        
+    
 }
