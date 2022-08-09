33d89eb84550dac44f9beffbe3e681e81927eb5f
Ali Kolukirik <aakolukirik@gmail.com>
2022-08-09 20:28:53 +0300

    F1PilotsViper

---
+++ Practices/F1PilotsViper/F1PilotsViper/Core/Modules/Detail Page/DetailPageEntity.swift
@@ -0,0 +1,14 @@
+//
+//  DetailPageEntity.swift
+//  F1PilotsViper
+//
+//  Created by ali on 6.08.2022.
+//
+
+import Foundation
+
+struct PilotDetail: Codable {
+    var id, age: Int?
+    var image: String?
+    var team: String?
+}
